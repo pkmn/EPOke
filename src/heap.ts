d4f359ccc40bddbac28c2ce555570d860de5fd8e
Kirk Scheibelhut <kjs@google.com>
2019-06-28 17:20:36 -0700

    Add heap

--- src/heap.ts
+++ src/heap.ts
@@ -1,49 +1,157 @@
-export const Comarators {
-  Max: (a: number, b: number) => b - a;
-  Min: (a: number, b: number) =>  a - b;
-}
-
-class Node<T> {
+interface Node<T> {
   val: T;
   weight: number;
 }
 
-export class Heap<T> {
-  cmp: (a: number, b: number) => number;
-  data: Node<T>[];
-  limit = 0;
+export const Comparators = {
+  Min: <T extends {}>(a: Node<T>, b: Node<T>) => a.weight - b.weight,
+  Max: <T extends {}>(a: Node<T>, b: Node<T>) => b.weight - a.weight,
+}
 
-  top(n: number = 1): T[] {
-    if (this.data.length == 0 || n <= 0) return [];
-    if (this.data.length === 1 && n === 1) return this.data[0];
-    if (n >= this.data.length) {
+export class Heap<T> {
+  private readonly cmp: (a: Node<T>, b: Node<T>) => number;
+  private readonly data: Array<Node<T>> = [];
+  private readonly limit: number;
+
+  constructor(cmp: (a: Node<T>, b: Node<T>) => number, limit = 0) {
+    this.cmp = cmp;
+    this.limit = limit;
+  }
+  
+  get length() {
+    return this.data.length;
+  }
+
+  toArray() {
+    return this.data.slice(0);
+  }
+
+  toString() {
+    return this.data.toString();
+  }
+
+  peek() {
+    return this.data[0];
+  }
+
+  pop() {
+    const pop = this.data.pop()
+    if (this.length > 0 && pop !== undefined) {
+      return this.replace(pop)
+    }
+    return pop
+  }
+
+  push(...data: Array<Node<T>>): boolean {
+    if (data.length < 1) return false;
+    if (data.length === 1) {
+      this.bubbleUp(this.data.push(data[0]) - 1);
+    } else {
+      let i = this.length;
+      this.data.push(...data);
+      for (const length = this.length; i < length; ++i) {
+        this.bubbleUp(i);
+      }
+    }
+    this.trim();
+    return true
+  }
+ 
+  replace(node: Node<T>) {
+    const peek = this.peek();
+    this.data[0] = node;
+    this.bubbleDown(0);
+    return peek;
+  }
+
+  remove(t: T) {
+    if (!this.length) return false;
+    if (t === undefined) {
+      this.pop()
+      return true
+    }
+    const i = this.data.findIndex(n => n.val === t);
+    if (i < 0) return false;
+    if (i === 0) {
+      this.pop()
+    } else if (i === this.length - 1) {
+      this.data.pop();
+    } else {
+      this.data.splice(i, 1, this.data.pop()!)
+      this.bubbleUp(i);
+      this.bubbleDown(i);
+    }
+    return true;
+  }
+
+  top(n: number = 1) {
+    if (this.length === 0 || n <= 0) return [];
+    if (this.length === 1 && n === 1) return [this.peek()];
+    if (n >= this.length) {
       const cloned = this.data.slice(0);
       cloned.sort(this.cmp);
       return cloned;
     }
-
-     // Use an inverted heap
-    const topHeap = new Heap((a, b) = > -1 * this.cmp(a, b));
-    topHeap.limit = n
+    const heap = new Heap((a: Node<T>, b: Node<T>) => -1 * this.cmp(a, b), n);
     const indices = [0]
-    const arr = this.heapArray
     while (indices.length) {
-      const i = indices.shift() as number
-      if (i < arr.length) {
-        if (topHeap.length < n) {
-          topHeap.push(arr[i])
-          indices.push(...Heap.getChildrenIndexOf(i))
-        } else if (this.compare(arr[i], topHeap.peek() as T) <= 0) {
-          topHeap.replace(arr[i])
-          indices.push(...Heap.getChildrenIndexOf(i))
+      const i = indices.shift()!;
+      if (i < this.length) {
+        if (heap.length < n) {
+          heap.push(this.data[i]);
+          indices.push(...Heap.indicesOfChildren(i));
+        } else if (this.cmp(this.data[i], heap.peek()) <= 0) {
+          heap.replace(this.data[i]);
+          indices.push(...Heap.indicesOfChildren(i));
         }
       }
     }
-    return topHeap.toArray()
+    return heap.toArray()
   }
 
-  private applyLimit() {
+  private bubbleUp(i: number) {
+    if (!i) return false;
+    while (true) {
+      const pi = Heap.indexOfParent(i);
+      if (pi < 0 || this.cmp(this.data[pi], this.data[i]) <= 0) break;
+      [this.data[i], this.data[pi]] = [this.data[pi], this.data[i]];
+      i = pi;
+    }
+    return true;
   }
 
+  private bubbleDown(i: number) {
+    if (i >= this.data.length - 1) return false;
+    const self = this.data[i]
 
+    while (true) {
+      const children = Heap.indicesOfChildren(i)
+      let ci = children[0];
+      for (let i = 1; i < children.length; i++) {
+        ci = (this.cmp(this.data[children[i]], this.data[ci]) < 0) ? children[i] : ci;
+      }
+      if (typeof this.data[ci] === 'undefined' || this.cmp(self, this.data[ci]) <= 0) break;
+      [this.data[i], this.data[ci]] = [this.data[ci], this.data[i]];
+      i = ci;
+    }
+    return true;
+  }
+
+  private trim() {
+    if (this.limit && this.limit < this.length) {
+      let rm = this.length - this.limit
+      while (rm--) this.data.pop();
+    }
+  }
+
+  static indicesOfChildren(index: number) {
+    return [index * 2 + 1, index * 2 + 2];
+  }
+
+  static indexOfParent(index: number) {
+    if (index <= 0) return -1;
+    const child = index % 2 ? 1 : 2
+    return Math.floor((index - child) / 2)
+  }
+}
 
