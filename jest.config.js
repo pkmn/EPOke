10bf4b31fad653ff1b8479566796a2c5b0ae37ef
Kirk Scheibelhut <kjs@google.com>
2020-02-26 14:48:19 -0800

    more dist->build, package.json, delete bin

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/', '<rootDir>/data/sets/' ],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/', '<rootDir>/data/sets/' ]
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/data/sets/' ]
 };
