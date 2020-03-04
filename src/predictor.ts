8e96d512727fa935f5c994e65f68174c8864a415
Kirk Scheibelhut <kjs@google.com>
2020-03-03 19:07:42 -0800

    Working predictor without real heuristics?

--- src/predictor.ts
+++ src/predictor.ts
@@ -5,24 +5,28 @@ import { Pool } from './pool';
 import { Random } from './random';
 
 interface Heuristics {
-  update(set: PokemonSet);
-  species() => 
-
+  // NOT Species | Species
+  update(set: PokemonSet) ;
+  // Species | Species
+  species(...species: string[]) => (k: string, v: number) => number; 
   spread(set: Partial<PokemonSet>): (s: StatsTable, v: number) => number; 
   ability(set: Partial<PokemonSet>): (k: string, v: number) => number 
   item(set: Partial<PokemonSet>) => (k: string, v: number) => number; 
+  // NOT Move | Move
   moves(set: Partial<PokemonSet>) => (k: string, v: number) => number; 
+  // Move | Move
   move(...move: string[]) => (k: string, v: number) => number; 
 }
 
 const AFN = (k: any, v: number) => v; 
 const AHEURISTIC: Heuristics = {
-  species: species => (k, v) => (species.includes(k) ? -1 : v);
+  update: () => {},
+  species: species => (k, v) => (species.includes(k) ? -1 : v),
   spread: () => AFN,
   ability: () => AFN,
   item: () => AFN,
   moves: () => AFN,
-  move: moves => (k, v) => (moves.includes(k) ? -1 : v);
+  move: moves => (k, v) => (moves.includes(k) ? -1 : v),
 };
 
 export class Predictor {
@@ -44,37 +48,39 @@ export class Predictor {
   }
 
   // PRECONDITION: possibilities has no gaps
-  // POSTCONDITION: possibilities is unmodified
-  predictTeam(possibilities: SetPossibilities[], random?: Random, validate = 0, size = 6) {
-    let species = this.species;
-    let i = 0;
+  // POSTCONDITION: possibilities and its elements are unmodified
+  predictTeam(possibilities: SetPossibilities[], random?: Random, validate = 0) {
+    const H = AHEURISTIC; // TODO: real heuristics
 
+    let species = this.species;
+
+    let last: PokemonSet | null = null;
     const team: PokemonSet[] = [];
-    while (this.team.length < size) {
-      if (possibilities[i]) {
-        const p = possiblities[i];
-        const set = this.predictSet(p, random);
-        if (validate-- > 0 && !this.validate(team, set)) continue;
-        team.push(set);
-        heuristics.update(set);
-        i++;
+    while (this.team.length < 6) {
+      let set: PokemonSet;
+      if (possibilities[this.team.length]) {
+        set = this.predictSet(possibilities[i], random, H);
       } else {
-        const s = species.select(first ? AHEURISTIC.species(...team.map(s => s.species)) : AFN, random);
+        const fn = H.species(last ? last : ...team.map(s => s.species));
+        const s = species.select(fn, random);
         species = s[1];
-        first = false;
-        const p = SetPossibilites.create(this.dex, s[0])
-        const set = this.predictSet(p, random, true);
-        if (validate-- > 0 && !this.validate(team, set)) continue;
-        team.push(set);
-        if (this.team.length < size) heuristics.update(set);
+        const p = SetPossibilities.create(this.dex, s[0])
+        const set = this.predictSet(p, random, H);
+        last = set;
       }
+      if (validate-- > 0 && !this.validate(team, set)) {
+        last = null;
+        continue;
+      }
+      team.push(set);
+      if (this.team.length < size) H.update(set);
     }
 
     return team;
   }
 
-  // POSTCONDITION: possibilities is unmodified iff clobber = false
-  predictSet(p: SetPossibilities, random?: Random, heuristics: Heuristics, clobber = false;) {
+  // POSTCONDITION: possibilities is unmodified
+  predictSet(p: SetPossibilities, random?: Random, H: Heuristics) {
     const set: Partial<PokemonSet> = {
       species: p.species,
       level: p.level,
@@ -83,24 +89,21 @@ export class Predictor {
       item: p.item || '',
       moves: p.moves.locked.slice(),
     };
-    const spread = p.spread.select(AHEURISTIC.spread(set), random)[0];
+    const spread = p.spread.select(H.spread(set), random)[0];
     set.nature: spread.nature?.id!;
     set.ivs: spread.ivs!;
     set.evs: spread.evs!;
 
-    if (!set.ability) set.ability = p.abilities.select(AHEURISTIC.ability(set), random)[0] || '';
-    if (!set.item) set.item = p.items.select(AHEURISTIC.item(set), random)[0] || '';
-    if (set.moves.length < 4) {
-      // TODO clibber fix
-      let moves = trample ? p.moves : p.moves.clone();
-      moves.update(AHEURISTIC.moves(set));
-      while (set.moves.length < 4) {
-        const m = p.moves.select(AHEURISTIC, random);
-        const move = m[1]
-        moves = m[2];
-        set.moves.push(move);
-        p.moves.update(AHEURISTIC.move(move));
-      }
+    if (!set.ability) set.ability = p.abilities.select(H.ability(set), random)[0] || '';
+    if (!set.item) set.item = p.items.select(H.item(set), random)[0] || '';
+
+    while (set.moves.length < 4) {
+      const fn = last ?  H.species(last) : combine(H.moves(set), H.move(...set.moves));
+      const m = p.moves.select(fn, random);
+      moves = m[1];
+      const move = m[0]
+      last = move;
+      set.moves.push(move);
     }
 
     const unhappy = set.moves.includes('Frustration') && !set.moves.includes('Return');
