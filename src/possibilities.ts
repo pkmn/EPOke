7c8b77b32945e8de696f8c24e540d9e4f2e1d711
Kirk Scheibelhut <kjs@google.com>
2020-03-03 19:08:53 -0800

    Remove types.ts

--- src/possibilities.ts
+++ src/possibilities.ts
@@ -1,10 +1,15 @@
-import { Dex, Species, StatsTable, Generation, Gender } from 'ps';
+import { Dex, ID,Species, StatsTable, Generation, Gender } from 'ps';
 import { DisplayUsageStatistics } from '@smogon/stats'; // -> smogon
 
 import { Pool } from './pool';
 import { Random } from './random';
 import { Stats, StatsRange } from './stats';
-import { DeepReadonly } from './types';
+
+type DeepReadonly<T> = T extends primitive ? T : DeepReadonlyObject<T>;
+type primitive = string | number | boolean | undefined | null | Function | ID;
+type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
 
 export class SetPossibilities {
   readonly species: Species;
